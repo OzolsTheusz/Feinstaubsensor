f69f1cb5d505bb60246caf323e7119fa34a3e94b
OzolsTheusz <51407868+OzolsTheusz@users.noreply.github.com>
2019-10-14 17:17:26 +0200

    Update sub.cpp

--- Version 1.9.1/src/sub.cpp
+++ Version 1.9.1/src/sub.cpp
@@ -13,9 +13,9 @@
 #define MQTT_Fein25   "42nibbles/MQTT/FEINSTAUB/Fein25"
 #define MQTT_FeinTEMP "42nibbles/MQTT/FEINSTAUB/FeinTemp"
 #define MQTT_SLEEP    "42nibbles/MQTT/SLEEP"
- const char* ssid =         "hw1_gast"; //CHANGE!!!!!!!!!!!!!!!!
- const char* password =     "KeineAhnung"; //CHANGE!!!!!!!!!!!!
- const char* mqtt_server =  "dz-pi.hw1.fb4.fh"; //CHANGE!!!!!!!!!!!!!!!!!!
+ const char* ssid =         "PWD_HERE"; //CHANGE!!!!!!!!!!!!!!!!
+ const char* password =     "SSID_HERE"; //CHANGE!!!!!!!!!!!!
+ const char* mqtt_server =  "SERVER_ADRESS_HERE"; //CHANGE!!!!!!!!!!!!!!!!!!
  //ErsetzungsPunkt für MQTT
 
 /*
